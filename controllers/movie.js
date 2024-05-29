78233f2d504b32393a6c45f4d6b96906b7fa32c0
Nicholas Keen <nicknack2019@yahoo.com>
2024-05-29 08:20:58 -0600

    should be functional

---
+++ controllers/movie.js
@@ -0,0 +1,86 @@
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res, next) => {
+  const result = await mongodb.getDb().db('movies').collection('movies').find();
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  });
+};
+
+const getSingle = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db('movies').collection('movies').find({ _id: userId });
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]);
+  });
+};
+
+const createMovie = async (req, res) => {
+  const movie = {
+    name: req.body.name,
+    genre:req.body.genre,
+    rating: req.body.rating,
+    year:req.body.year,
+    boxOffice:req.body.boxOffice,
+    rottenTomatoesScore:req.body.rottenTomatoesScore,
+    streamingServices:req.body.streamingServices
+  };
+  
+  const response = await mongodb.getDb().db('movies').collection('movies').insertOne(movie);
+  console.log(response)
+  if (response.acknowledged){
+    res.status(201).json(response);
+  } else{
+    res.status(500).json(response.error || 'some error occured while creating the movie.');
+  }
+  };
+
+  const updateContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+
+    const movie = {
+      name: req.body.name,
+      genre:req.body.genre,
+      rating: req.body.rating,
+      year:req.body.year,
+      boxOffice:req.body.boxOffice,
+      rottenTomatoesScore:req.body.rottenTomatoesScore,
+      streamingServices:req.body.streamingServices
+    };
+    const response = await mongodb
+    .getDb()
+    .db('movies')
+    .collection('movies')
+    .replaceOne({_id: userId }, movie);
+    console.log(response);
+    if (response.modifiedCount > 0){
+      res.status(204).send();
+    } else{
+      res.status(500).json(response.error || "some error occured while updating the movie. ");
+    }
+    };
+
+    const deleteContact = async (req,res) => {
+      const userId = new ObjectId(req.params.id);
+      const response = await mongodb.getDb().db('movies').collection('movies').deleteOne({_id: userId},true);
+      console.log(response);
+      if (response.deletedCount > 0) {
+        res.status(204).send();
+      } else {
+        res.status(500).json(response.error || 'Some error ocurred while deleting movie. ');
+      }
+      }
+    
+
+
+
+module.exports = { 
+  getAll, 
+  getSingle,
+createContact,
+updateContact,
+deleteContact
+ };
---
+++ routes/movies.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const contactsController = require('../controllers/contacts');
+
+router.get('/', contactsController.getAll);
+
+router.get('/:id', contactsController.getSingle);
+
+router.post('/', contactsController.createMovie);
+
+router.put('/:id', contactsController.updateMovie);
+
+router.delete('/:id', contactsController.deleteMovie);
+
+
+module.exports = router;
