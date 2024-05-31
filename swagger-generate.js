07c2154d875dd90c42debace07561fcb02d4260b
Nicholas Keen <nicknack2019@yahoo.com>
2024-05-31 17:46:01 -0600

    changed host in swagger-generate.js to movies and not contacts

--- swagger-generate.js
+++ swagger-generate.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Description'
   },
-  host: [ 'https://cse-341-1.onrender.com/contacts/']
+  host: [ 'https://cse-341-1.onrender.com/movies/']
 };
 
 const outputFile = './swagger-output.json';
